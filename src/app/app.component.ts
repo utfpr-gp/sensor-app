3ac5980dd73495ec302311a4e0ddbe2d0fe1abf9
Roni Fabio Banaszewski <roni.banaszewski@gmail.com>
2021-06-24 17:54:22 -0300

    Added reactive notification.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'sensor-app';
+
+  ready: boolean = false;
+  active: boolean = true;
+
+  private readySubject = new BehaviorSubject<boolean>(this.ready);
+  private activeSubject = new BehaviorSubject<boolean>(this.active);
+  private timesSubject = new BehaviorSubject<number>(0);
+
+  ngOnInit() {
+    this.readySubject.subscribe(value => {
+      this.ready = value;
+      this.rule()
+    });
+
+    this.activeSubject.subscribe(value => {
+      this.active = value;
+      this.rule()
+    });
+  }
+
+  rule() {
+    if (this.ready == false && this.active == true) {
+      console.log("Executou!");
+      this.readySubject.next(true);
+      this.activeSubject.next(false);
+    }
+  }
+
 }
